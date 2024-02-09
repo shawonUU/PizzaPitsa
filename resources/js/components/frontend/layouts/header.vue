3ef68075f7b677ec6d54f338471bec6279ae1db0
devsozib <mdsajibhassan01993@gmail.com>
2024-02-09 21:44:03 +0600

    working on forntend

--- resources/js/components/frontend/layouts/header.vue
+++ resources/js/components/frontend/layouts/header.vue
@@ -32,19 +32,75 @@
                     </div>
                 </div>
             </div>
+            <header class="sc-1of5u0p-0 guZDAe container">
+                <div class="left">
+                    <a class="sc-2c0aw7-0 wwSTC sc-1of5u0p-1 ibZIbo" href="/en/tallinn" data-active="true" data-type="primary" data-size="normal">
+                       <img  width="100px" :src="'/frontend/assets/images/logo/pizza logo-02.jpg'" alt="Site Logo">
+                    </a>
+                    <div class="sc-yrfxdq-0 bzQMgF header__about">
+                    <span class="header__about-slogan">
+                        <span class="header__about-slogan-text">Pizza delivery </span>
+                        <a data-testid="header__about-slogan-text_link" class="header__about-slogan-text header__about-slogan-text_locality header__about-slogan-text_link" href="#">Tallinn</a>
+                    </span>
+                    <div>
+                        <div class="sc-1ohwd10-0 fafGiC">39 min <div class="sc-1ohwd10-1 lbkYqN">
+                            <i class="sc-1ohwd10-2 gOUAXw"></i>
+                            <div from="opacity,0,transform,translateY(20px)" to="opacity,1,transform,translateY(0)" class="sc-xk8uwh-0 eWuxAR"></div>
+                        </div>4.85 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="sc-1ohwd10-3 bjNpWs">
+                            <defs>
+                            <linearGradient id="star_16_svg__a">
+                                <stop offset="50%" stop-color="#FFD200"></stop>
+                                <stop offset="50%" stop-color="#999" stop-opacity="0.5"></stop>
+                            </linearGradient>
+                            </defs>
+                            <path fill="url(#star_16_svg__a)" d="M8.451 1.49a1 1 0 00-.902 0c-.245.123-.378.359-.461.528-.09.182-.185.427-.296.712l-.928 2.39a3.374 3.374 0 01-.07.173v.002H5.79c-.036.006-.086.01-.184.02l-2.504.214c-.272.024-.51.044-.695.077-.176.032-.418.09-.6.274a1 1 0 00-.28.826c.03.256.186.45.307.583.126.139.302.3.503.485l1.987 1.823.125.118.002.002v.003c-.006.033-.016.079-.036.168l-.592 2.66a9.167 9.167 0 00-.145.73c-.024.184-.042.445.087.68a1 1 0 00.733.508c.265.038.504-.072.667-.16a9.15 9.15 0 00.632-.392l2.036-1.332c.086-.056.13-.085.164-.104L8 12.476l.003.002c.033.019.078.048.164.104l2.036 1.332c.246.161.458.3.632.393.163.087.401.197.667.159a1 1 0 00.733-.508c.13-.235.11-.496.087-.68a9.199 9.199 0 00-.145-.73l-.592-2.66c-.02-.09-.03-.135-.035-.168v-.003l.001-.002.125-.118 1.987-1.823c.201-.185.377-.346.503-.485.12-.133.276-.327.308-.583a1 1 0 00-.281-.826c-.182-.183-.424-.242-.6-.274-.185-.033-.423-.053-.695-.077l-2.504-.215a3.372 3.372 0 01-.184-.018h-.003l-.002-.003a3.421 3.421 0 01-.069-.172l-.928-2.39a9.644 9.644 0 00-.296-.713c-.083-.17-.216-.405-.46-.529z"></path>
+                        </svg>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="sc-52lbjw-0 fpBABM contacts-phone">
+                    <div class="contacts-phone__number">
+                        <a class="contacts-phone__number-link" href="tel:6299209">6 299 209</a>
+                    </div>
+                    <div class="contacts-phone__description">
+                        <span class="contacts-phone__description-text">Call</span>
+                    </div>
+                    </div>
+                </div>
+                <div class="right">
+                    <a class="sc-2c0aw7-0 llYDFl sc-1of5u0p-2 jzJZpw" href="/en/tallinn/loyaltyprogram" data-active="false" data-type="primary" data-size="normal">
+                    <span class="icon">
+                        <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M11 1a1 1 0 011 1v2a8 8 0 110 16v2a1 1 0 11-2 0v-2H6.6c-.56 0-.84 0-1.05-.1a1 1 0 01-.44-.45C5 19.24 5 18.96 5 18.4V5.6c0-.56 0-.84.1-1.05a1 1 0 01.45-.44C5.76 4 6.04 4 6.6 4H10V2a1 1 0 011-1zm1 17a6 6 0 000-12H7v12h5z" fill="#000"></path>
+                        </svg>
+                    </span>
+                    <div class="header-action">
+                        <ul class="action-list">                                
+                            <li class="shopping-cart">
+                                <a href="#" class="cart-dropdown-btn">
+                                    <span class="cart-count">3</span>
+                                    <i class="flaticon-shopping-cart"></i>
+                                </a>
+                            </li>                                
+                        </ul>
+                    </div>                        
+                    </a>
+                    <button data-testid="header_login" type="button" data-type="tertiary" data-size="small" class="sc-1rmt3mq-0 dOEDNV">Log in</button>
+                </div>
+            </header>
             <!-- Start Mainmenu Area  -->
             <div id="axil-sticky-placeholder" style="height: 0px;"></div>
             <div class="axil-mainmenu">
                 <div class="container">
                     <div class="header-navbar">
-                        <div class="header-brand">
+                        <!-- <div class="header-brand">
                             <a href="index-2.html" class="logo logo-dark">
                                 <img  width="100px" :src="'/frontend/assets/images/logo/pizza logo-02.jpg'" alt="Site Logo">
                             </a>
                             <a href="index-2.html" class="logo logo-light">
                                 <img width="200px" :src="'/frontend/assets/images/logo/pizza logo-02.jpg'" alt="Site Logo">
                             </a>
-                        </div>
+                        </div> -->
                         <div class="header-main-nav">
                             <!-- Start Mainmanu Nav -->
                             <nav class="mainmenu-nav">
@@ -54,19 +110,20 @@
                                         <img :src="'/frontend/assets/images/logo/pizza logo-02.jpg'" alt="Site Logo">
                                     </a>
                                 </div>
-                                <ul class="mainmenu">
+                                <!-- <ul class="mainmenu">
                                     <li>
                                         <a href="#">Home</a>                                   
                                     </li>                               
                                     <li><a href="contact.html">Contact</a></li>
-                                </ul>
+                                </ul> -->
+                                <ul class="mainmenu"><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Combo</a></li><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Pizza</a></li><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Snacks</a></li><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Desserts</a></li><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Coffee</a></li><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Drinks</a></li><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Sauces</a></li><li class="sc-xlo7eb-4 bvuzKi"><a class="sc-2c0aw7-0 wwSTC sc-xlo7eb-7 kkaUZR" data-active="false" data-type="primary" data-size="normal">Other goods</a></li></ul>
                             </nav>
                             <!-- End Mainmanu Nav -->
                         </div>
                         <div class="header-action">
                             <ul class="action-list">
                                 
-                                <li class="axil-search d-xl-none d-block">
+                                <!-- <li class="axil-search d-xl-none d-block">
                                     <a href="javascript:void(0)" class="header-search-icon" title="Search">
                                         <i class="flaticon-magnifying-glass"></i>
                                     </a>
@@ -77,32 +134,9 @@
                                         <span class="cart-count">3</span>
                                         <i class="flaticon-shopping-cart"></i>
                                     </a>
-                                </li>
-                                <li class="my-account">
-                                    <a href="javascript:void(0)">
-                                        <i class="flaticon-person"></i>
-                                    </a>
-                                    <div class="my-account-dropdown">
-                                        <span class="title">QUICKLINKS</span>
-                                        <ul>
-                                            <li>
-                                                <a href="my-account.html">My Account</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Initiate return</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Support</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Language</a>
-                                            </li>
-                                        </ul>
-                                        <div class="login-btn">
-                                            <a href="sign-in.html" class="axil-btn btn-bg-primary">Login</a>
-                                        </div>
-                                        <div class="reg-footer text-center">No account yet? <a href="sign-up.html" class="btn-link">REGISTER HERE.</a></div>
-                                    </div>
+                                </li> -->
+                                <li>                                    
+                                    <button data-testid="header_login" type="button" data-type="tertiary" data-size="small" class="sc-1rmt3mq-0 dOEDNV">My Orders</button>                                                                    
                                 </li>
                                 <li class="axil-mobile-toggle">
                                     <button class="menu-btn mobile-nav-toggler">
@@ -131,3 +165,162 @@ export default {
 }
 }
 </script>
+
+<style scoped>
+.guZDAe {
+    display: flex;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    padding: 24px 0px 16px;
+    z-index: 100;
+    width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.guZDAe .left {
+    display: flex;
+}
+.guZDAe .left > :nth-child(n) {
+    margin-right: 40px;
+}
+.guZDAe .left > :nth-child(n) {
+    margin-right: 40px;
+}
+.bzQMgF {
+    position: relative;
+}
+
+.bzQMgF .header__about-slogan {
+    color: rgb(0, 0, 0);
+    font-size: 18px;
+    line-height: 1.2;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+    margin: 0px;
+}
+.bzQMgF .header__about-slogan-text {
+    display: inline-block;
+    white-space: pre-wrap;
+}
+.bzQMgF .header__about-slogan {
+    color: rgb(0, 0, 0);
+    font-size: 18px;
+    line-height: 1.2;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+    margin: 0px;
+}
+.bzQMgF .header__about-slogan-text_link {
+    color: rgb(255, 105, 0);
+    text-decoration: none;
+}
+.bzQMgF .header__about-slogan-text_locality {
+    white-space: normal;
+}
+
+.bzQMgF .header__about-slogan-text {
+    display: inline-block;
+    white-space: pre-wrap;
+}
+.fafGiC {
+    position: relative;
+    margin-top: 1px;
+    color: rgb(0, 0, 0);
+    font-size: 15px;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+    z-index: 2;
+}
+.lbkYqN {
+    position: relative;
+    display: inline-block;
+    z-index: 2;
+}
+.bjNpWs {
+    vertical-align: -2px;
+    margin-left: 2px;
+}
+.guZDAe .left > :nth-child(n) {
+    margin-right: 40px;
+}
+.fpBABM {
+    position: relative;
+}
+.fpBABM .contacts-phone__number {
+    white-space: nowrap;
+    font-size: 18px;
+    line-height: 1.2;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+}
+.fpBABM .contacts-phone__number-link {
+    color: rgb(0, 0, 0);
+    text-decoration: none;
+}
+.fpBABM .contacts-phone__description {
+    margin-top: 4px;
+    color: rgb(153, 153, 153);
+    font-size: 15px;
+    line-height: 1;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+}
+.fpBABM .contacts-phone__description-text {
+    display: inline-block;
+}
+.fpBABM .contacts-phone__description {
+    margin-top: 4px;
+    color: rgb(153, 153, 153);
+    font-size: 15px;
+    line-height: 1;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+}
+.guZDAe .right {
+    display: flex;
+    column-gap: 32px;
+    -webkit-box-align: center;
+    align-items: center;
+}
+.jzJZpw {
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    -webkit-box-align: center;
+    align-items: center;
+}
+.llYDFl {
+    text-decoration: none;
+    cursor: pointer;
+    outline: none;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 600;
+    color: rgb(255, 105, 0);
+}
+.dOEDNV[data-size="small"] {
+    height: 32px;
+    padding: 8px 16px;
+    font-size: 14px;
+    line-height: 16px;
+}
+.dOEDNV[data-type="tertiary"] {
+    background-color: rgb(243, 243, 247);
+    color: rgb(92, 99, 112);
+}
+.dOEDNV {
+    outline: none;
+    border: none;
+    border-radius: 9999px;
+    text-align: center;
+    font-family: Dodo, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-weight: 500;
+    text-decoration: none;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+    user-select: none;
+    transition-property: background-color, color;
+    transition-duration: 200ms;
+    transition-timing-function: ease-out;
+}
+</style>
