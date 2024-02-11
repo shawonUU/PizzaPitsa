2afe51cf5dcafa696e40982ffbc69e1981da1a3d
devsozib <mdsajibhassan01993@gmail.com>
2024-02-11 21:49:58 +0600

    Working for add product

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/', function () {
 Route::prefix('admin')->middleware(['auth'])->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
     Route::get('category', [ProductMnagementController::class, 'category'])->name('category');    
+    // Route::resource('product')
 });
 
 
